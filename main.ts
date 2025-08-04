2a18f1f84bfb5e842dbd899967ccdd67802c9101
ecnryptionStudios <calvin.heiniger@outlook.com>
2025-08-04 08:02:47 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
+namespace hexscreen {
+    const CHANNEL = "hexscreen"
 
+    function sendJSON(json: any) {
+        const msg = JSON.stringify(json)
+        const buf = Buffer.fromUTF8(msg)
+        control.simmessages.send(CHANNEL, buf)
+    }
+
+    /**
+     * Sets a simulated pixel at (x, y) to the given hex color.
+     */
+    //% block
+    export function setPixel(x: number, y: number, hexColor: string) {
+        sendJSON({
+            action: "setPixel",
+            x,
+            y,
+            hex: hexColor
+        })
+    }
+}
